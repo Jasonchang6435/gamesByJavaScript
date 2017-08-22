bcb2e1a4ae294f4f9fd8cb71e11fd7d6767f40e9
jason <643501654@qq.com>
2017-08-22 22:39:27 +0800

    delete gua label

--- hit_planes/guagame/gua_particle_system.js
+++ hit_planes/guagame/gua_particle_system.js
@@ -1,22 +1,22 @@
-class GuaLabel {
-    constructor(game,text) {
-        this.game = game
-        this.text = text
-
-    }
-
-    static new(game,text) {
-        return new this(game,text)
-    }
-
-    draw() {
-        this.game.context.fillText(this.text, 100, 190)
-    }
-
-    update() {
-
-    }
-}
+// class GuaLabel {
+//     constructor(game,text) {
+//         this.game = game
+//         this.text = text
+//
+//     }
+//
+//     static new(game,text) {
+//         return new this(game,text)
+//     }
+//
+//     draw() {
+//         this.game.context.fillText(this.text, 100, 190)
+//     }
+//
+//     update() {
+//
+//     }
+// }
 
 class GuaParticle extends GuaImage {
     constructor(game) {
